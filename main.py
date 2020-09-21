8db50b78c030c7a517f9f359b53658d7953e5228
mm <martin@marshtrio.co.uk>
2020-09-21 12:54:47 +0100

    Nmea message corrections - autohelm basic logic

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def log(boat_data: dict, q: asyncio.Queue):
     while True:
         await asyncio.sleep(10)
         async with AIOFile("./logs/log.txt", 'a+') as afp:
-            line = "".join([json.dumps(boat_data),",\n"])
+            line = "".join([json.dumps(boat_data), ",\n"])
             print(line)
             await afp.write(line)
             await afp.fsync()
@@ -48,33 +48,22 @@ async def autohelm(boat_data: dict, q: asyncio.Queue):
     last_heading = None
     while True:
         await asyncio.sleep(.2)
-        heading = b.read_compass()
+        heading = b.read_compass()  # heading is *10 deci-degrees
         if last_heading is None:
             last_heading = heading
         items[c] = heading
-        if not (c % 100):
-            total = 0
-            # Every 100 *.2  ie 20s set desired heading
-            for x in range(0, 100):
-                cx = c - x
-                val = items.get(loop(cx), 0)
+        hts = int((boat_data.get('hts') + boat_data.get('mag_var'))*10)
 
-                if val > 1800:
-                    val -= 3600
-                total += val
-
-            cts = total / 100
-            if cts < 0:
-                cts += 3600
-
-            print(f'***************  course to steer {cts/10}')
-
-        c = loop(c + 1)
         # desired turn rate is compass error  / no of secs
-        error = relative_direction(heading - cts)
-        turn_rate = relative_direction(heading - last_heading) * 3
-        correction = (turn_rate + error) * 50
-        print(f'heading {heading/10}  cts {cts / 10}  correction {correction}')
+        error = relative_direction(heading - hts)
+        turn_rate = relative_direction(heading - last_heading)
+
+        # Desired turn rate is 10 degrees per second ie  2 per .2s or 20 deci-degrees
+        desired_rate = error / 20
+        gain = 50
+        correction = (turn_rate + desired_rate) * gain
+        print(f'heading {heading/10}  hts {hts / 10} turn rate {turn_rate} '
+              f'error {error}  desired {desired_rate} correction {correction}')
         b.helm(correction)
         last_heading = heading
 
@@ -91,7 +80,7 @@ def get_micro_secs(real_str: str) -> int:
     If there is a decimal point returns fractional part as an integer
     based on 10 to minus 6 ie if seconds returns microseconds
     :param real_str:  A string with optional fraction
-    :return: decimal par as integer based on micro units eg microseconds
+    :return: decimal part as integer based on micro units eg microseconds
     """
     try:
         p1, p2 = real_str.split(".")
@@ -102,8 +91,32 @@ def get_micro_secs(real_str: str) -> int:
         return int(p2)
     return 0
 
+def to_true(amount: float, flag: str, mag_var: float) -> float:
+    """
+    Returns True bearing or Course
+    :param amount: value of direction in degrees
+    :param flag: T = True M = Magnetic
+    :param mag_var:  variation to correct magnetic values
+    :return: True value
+    """
+    value = amount
+    if flag == 'M':
+        value = amount - mag_var
+    return value
 
-def get_nmea_field_value(value_fields, format_def):
+
+def get_nmea_field_value(value_fields: list, format_def: tuple, mag_var: float) -> object:
+    """ Returns a single value (str, int, float) based on a key which selects how the fields are processed.
+    Given a extracted portion of a NME0183 sentence (each comma separated field sent in a list)
+    we convert the strings there into a single value and return it. Some conversions require a single
+    field whilst datetime, lat and long etc require many.
+    The format def is really a key to defines which lambda function we will use; see func_map. The
+    key is written in a format which helps the developer to write and check the function.
+    :param value_fields: list of value strings to be converted into a single value
+    :param format_def: key to select the appropriate function
+    :param mag_var: Magnetic Variation for conversion true to magnetic
+    :return: the computed value
+    """
     value = None
     func_map = {
         "hhmmss.ss": lambda value_list: datetime.time(
@@ -112,7 +125,7 @@ def get_nmea_field_value(value_fields, format_def):
         "yyyyy.yyyy,a": lambda value_list: ((float(value_list[0][0:3]) + float(value_list[0][3:])/60.0)
                                             * sign_nmea(value_list[1], {'E': 1, 'W': -1})),
         "llll.llll,a": lambda value_list: ((float(value_list[0][0:2]) + float(value_list[0][2:])/60.0)
-                                            * sign_nmea(value_list[1], {'N': 1, 'S': -1})),
+                                           * sign_nmea(value_list[1], {'N': 1, 'S': -1})),
         "x.x": lambda value_list: float(value_list[0]),
         "ddmmyy": lambda value_list: datetime.date(
             day=int(value_list[0][0:2]), month=int(value_list[0][2:4]), year=int(value_list[0][4:])+2000).isoformat(),
@@ -127,8 +140,7 @@ def get_nmea_field_value(value_fields, format_def):
         ).isoformat(),
         "x.x,R": lambda value_list: float(value_list[0]) * sign_nmea(value_list[1], {'R': 1, 'L': -1}),
         "s": lambda value_list: value_list[0],
-        "x.x,T": lambda value_list: value_list[0],   # To do convert to true  if 2nd field is magnetic ie M
-        "x.x,+x.x": lambda value_list: float(value_list[0]) + float(value_list[0])
+        "x.x,T": lambda value_list: to_true(value_list[0], value_list[1], mag_var)
 
     }
     func = func_map.get(format_def[1])
@@ -145,11 +157,15 @@ def get_nmea_field_value(value_fields, format_def):
     return value
 
 
-def get_sentence_data(sentence: str, var_names: list) -> dict:
+def get_sentence_data(sentence: str, var_names: list, mag_var: float) -> dict:
     """
     Gets a dict of extracted from the NMEA 0183 sentence as defined by var_names
+    note depth must be calculated by adding DBT and TOFF together then the measure will be from
+    keel or from waterline
+    HDM is always magnetic as defined by sentence HDM
     :param sentence - received NMEA 0183 sentence or line read
     :param var_names - list of variables to extract in processing order
+    :param mag_var: Magnetic Variation for conversion true to magnetic
     :return: variables and values extracted
     """
 
@@ -164,9 +180,16 @@ def get_sentence_data(sentence: str, var_names: list) -> dict:
         "mag_var": (2, "x.x,a"),        # Mag Var E positive
         "datetime": (6, "hhmmss.ss,dd,dd,yyyy,tz_h,tz_m"),  # Datetime from ZDA if available
         "XTE": (2, "x.x,R"),            # Cross Track Error R is positive
-        "XTE_units": (1, "A"),          # Units for XTE - N = NmH
+        "XTE_units": (1, "A"),          # Units for XTE - N = Nm
+        "ACir": (1, "A"),               # Arrived at way pt circle
+        "APer": (1, "A"),               # Perpendicular passing of way pt
+        "BOD": (2, "x.x,T"),            # Bearing origin to destination True
+        "Did": (1, "s"),                # Destination Waypoint ID as a str
+        "BPD": (2, "x.x,T"),            # Bearing, present position to Destination True
+        "HTS": (2, "x.x,T"),            # Heading to Steer True
         "HDM": (1, "x.x"),              # Heading Magnetic
-        "Depth": (2, "x.x,+x.x"),       # Depth below keel or waterline depending on value set
+        "DBT": (1, "x.x"),              # Depth below transducer
+        "TOFF": (1, "x.x"),             # Transducer offset -ve from transducer to keel +ve transducer to water line
         "STW": (1, "x.x"),              # Speed Through Water float knots
         "DW": (1, "x.x"),               # Water distance since reset float knots
     }
@@ -178,27 +201,28 @@ def get_sentence_data(sentence: str, var_names: list) -> dict:
         while x > 0 and fields:
             field_values.append(fields.pop(0))
             x -= 1
-        value = get_nmea_field_value(field_values, def_vars[var_name])
+        value = get_nmea_field_value(field_values, def_vars[var_name], mag_var)
         if value:
             sentence_data[var_name] = value
     return sentence_data
 
 
-def nmea_decoder(sentence: str, data: dict):
+def nmea_decoder(sentence: str, data: dict, mag_var: float):
     """
     Decodes a received NMEA 0183 sentence into variables and adds them to current data store
     :param sentence: received  NMEA sentence
     :param data: variables extracted
+    :param mag_var: Magnetic Variation for conversion true to magnetic
 
     """
 
     sentences = {
         "RMC": ["time", "status", "lat", "long", "SOG", "TMG", "date", "mag_var"],
         "ZDA": ["datetime"],
-        "APB": ["status", "", "XTE", "XTE_units", "ACir", "APer", "BOD", "Did", "HTS"],
+        "APB": ["status", "", "XTE", "XTE_units", "ACir", "APer", "BOD", "Did","BPD","HTS"],
         "HDG": ["", "", "", "mag_var"],
         "HDM": ["HDM"],  # 136.8, M * 25
-        "DPT": ["Depth"],  # 2.8, -0.7
+        "DPT": ["DBT", "TOFF"],  # 2.8, -0.7
         "VHW": ["", "", "", "", "STW"],  # T,, M, 0.0, N, 0.0, K
         "VLW": ["", "", "WD"],  # 23.2, N, 0.0, N
     }
@@ -208,7 +232,7 @@ def nmea_decoder(sentence: str, data: dict):
         if len(sentence) > 9:
             code = sentence[3:6]
             if code in sentences:
-                sentence_data = get_sentence_data(sentence, sentences[code])
+                sentence_data = get_sentence_data(sentence, sentences[code], mag_var)
                 if sentence_data.get('status', 'A') == 'A':
                     for n, v in sentence_data.items():
                         data[n] = v
@@ -219,10 +243,12 @@ def nmea_decoder(sentence: str, data: dict):
 
 async def nmea_reader(device_name: str, aioserial_instance: aioserial.AioSerial, boat_data: dict,
                       q_serial: asyncio.Queue, q_udp: asyncio.Queue):
+    mag_var = 0
     while True:
         line = await aioserial_instance.readline_async()
         line_str = line.decode(errors='ignore')
-        nmea_decoder(line_str, boat_data)
+        nmea_decoder(line_str, boat_data, mag_var)
+        mag_var = boat_data.get("mag_var", mag_var)
         await q_serial.put(line)
         await q_udp.put(line)
 
@@ -251,7 +277,7 @@ async def process_udp_queue(q: asyncio.Queue, ip: str, port: int):
     """
     Processes a queue sending each line to a UDP sever such as OpenCPN
     set up with ip address 0.0.0.0 and same port. This is fully async
-    non async udp would work with just:
+    a non async udp would work with just:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     sock.sendto(line, (ip, port))
     :param q:
@@ -302,7 +328,7 @@ async def main(consumers):
     q_gps = asyncio.Queue()
     q_udp = asyncio.Queue()
     producers = [
-        # asyncio.create_task(autohelm(boat_data, q_other)),
+        asyncio.create_task(autohelm(boat_data, q_other)),
         asyncio.create_task(log(boat_data, q_other)),
         asyncio.create_task(process_udp_queue(q_udp, "192.168.1.88", 8011)),
     ]   # producers write to queues to pass and multiplex nmea sentences to consumers
@@ -342,9 +368,9 @@ def cancel_consumers(consumer_list):
 
 
 if __name__ == "__main__":
-    consumers = []  # consumers read queues to pass receive multiplexed nmea sentences
+    q_readers = []  # consumers read queues to process/relay receive multiplexed nmea sentences
     try:
-        asyncio.run(main(consumers))
+        asyncio.run(main(q_readers))
     except (KeyboardInterrupt, BaseException) as e:
         print(e)
-        cancel_consumers(consumers)
+        cancel_consumers(q_readers)
